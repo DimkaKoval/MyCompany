f2641a3e3ea15207d1d81e3e0a585e5a1c4441a1
OlegShevtsiv <38689529+OlegShevtsiv@users.noreply.github.com>
2020-12-21 13:07:57 +0200

    Add project files.

---
+++ MyCompany/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Заповніть назву послуги")]
+        [Display(Name = "Назва послуги")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Коротки опис")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "повний опис")]
+        public override string Text { get; set; }
+    }
+}
---
+++ MyCompany/Domain/Entities/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class TextField : EntityBase
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Назва сторінки")]
+        public override string Title { get; set; } = "інформаційна сторінка";
+
+        [Display(Name = "Наповнення сторінки")]
+        public override string Text { get; set; } = "Текст яким наповнює адмін";
+    }
+}
